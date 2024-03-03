66bbeb8d185fac787f852a220c17643ab6355946
Falren <nokuchie@gmail.com>
2024-03-03 23:51:34 +0200

    refactor add rspec

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,16 @@
 class WelcomeController < ApplicationController
   def index
-    counter = Counter.find_or_create_by(name: "default")
-    counter.value = (counter.value || 0) + 1
-    counter.save!
-    render plain: "Counter #{counter.value}; #{Rails.version}/#{RUBY_VERSION}"
+    update_counter
+    render plain: "Counter #{current_counter.value}; #{Rails.version}/#{RUBY_VERSION}"
+  end
+
+  private
+
+  def current_counter
+    @current_counter ||= Counter.find_or_create_by(name: "default")
+  end
+
+  def update_counter
+    current_counter.increment!(:value)
   end
 end
